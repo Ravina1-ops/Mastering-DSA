33cb577592eb8102c64ffd736b71061bdeb9d6da
Ravina <hema130384@gmail.com>
2026-01-12 09:52:13 +0530

    Multiple Deuplicates program

--- Array/Multiple_Duplicates.cpp
+++ Array/Multiple_Duplicates.cpp
@@ -6,9 +6,9 @@ int arr[5]={1,3,3,1,2};
 int n=5;
 cout<<"Duplicates: "<<endl;
 for(int i = 0; i<n;i++){
-    int index = abs(arr[i]);// find absolute value |-5|=5
+    int index = abs(arr[i]) - 1;// find absolute value |-5|=5
     if(arr[index]<0){
-        cout<<index<<" ";
+        cout<<abs(arr[i])<<" ";
     }
     else{
         arr[index] = -arr[index];
