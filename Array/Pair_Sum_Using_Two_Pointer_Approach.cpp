d3c3b6e5d0ec702b0d1a3b64bbccee49d38fcc32
Ravina <hema130384@gmail.com>
2026-01-14 15:48:43 +0530

    solve pair sum by man mix

---
+++ Array/Pair_Sum_Using_Two_Pointer_Approach.cpp
@@ -0,0 +1,25 @@
+//   int arr[5]={1,2,3,4,5};
+//   int n = 5;
+//   int s = 5;
+//    int arr[5]={-2,-3,3,-3,2};
+  int n = 5;
+//   int s = 0;
+int arr[5] = { 2,2,2,2,3};
+int s =4;
+  int f = 0;
+  int l = n-1;
+  while(l>f){
+if(arr[f] + arr[l] == s){
+    cout<<arr[f]<<" "<<arr[l]<<endl;
+    f++,l--;
+}
+else {
+    if(arr[f] + arr[l]< s ){
+        f++;
+    }
+    else{
+        l--;
+    }   
+} 
+}
+}
