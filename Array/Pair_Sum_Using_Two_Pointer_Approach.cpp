2295dc39a660c16b1fc6b57e18653ba9a9859087
Ravina <hema130384@gmail.com>
2026-01-14 15:50:45 +0530

    Solving Pair Sum by Two Pointer aproach

--- Array/Pair_Sum_Using_Two_Pointer_Approach.cpp
+++ Array/Pair_Sum_Using_Two_Pointer_Approach.cpp
@@ -1,11 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
 //   int arr[5]={1,2,3,4,5};
 //   int n = 5;
 //   int s = 5;
 //    int arr[5]={-2,-3,3,-3,2};
-  int n = 5;
 //   int s = 0;
 int arr[5] = { 2,2,2,2,3};
 int s =4;
+ int n = 5;
   int f = 0;
   int l = n-1;
   while(l>f){
