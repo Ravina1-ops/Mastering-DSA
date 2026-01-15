de2693f95b0885434ed85306ed227182729e3bcc
Ravina <hema130384@gmail.com>
2026-01-15 21:46:16 +0530

    Sort 0 and 1 program

--- Array/Sort_0_and_1.cpp
+++ Array/Sort_0_and_1.cpp
@@ -1,24 +1,37 @@
 #include<iostream>
 using namespace std;
+void printarray(int arr[], int n){
+    for(int i = 0; i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+void sortarray(int arr[], int n){
+    int left = 0;
+    int right = n-1;
+    while(left<right)
+    {
+        while(arr[left]==0&&left<right){
+            left++;
+        }
+        while(arr[right]==1&&left<right){
+            right--;
+        }
+    
+    swap(arr[left],arr[right]);{
+        left++;
+        right--;
+    }
+    }
+}
 int main(){
     int arr[5] = {0,1,1,0,0};
-// int s =4;
- int n = 5;
-  int f = 0;
-  int l = n-1;
-  while(l>=f){
-if(arr[f] == 0 && arr[l] == 1){
-    cout<<arr[f]<<" "<<arr[l]<<endl;
-    f++,l--;
-}
-else {
-    if(arr[f] ==1 && arr[l]== 0 && l>f ){
-        swap(arr[f],arr[l]);
-        f++;
-    }
-    else{
-        l--;
-    }   
-} 
-}
+    int arr2[10] = {0,1,0,1,0,1,0,1,1,0};
+    int n = 5;
+    sortarray(arr, 5);
+    printarray(arr, 5);
+    cout<<endl;
+    sortarray(arr2, 10);
+    printarray(arr2, 10);
+
 }
