81c0d798f9c1a422a79cd8d2c1a2039e9d449616
Ravina <hema130384@gmail.com>
2026-01-17 19:07:18 +0530

    Binary Search

--- Binary Search/BinarySearch.cpp
+++ Binary Search/BinarySearch.cpp
@@ -1,29 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int binarySearch(int arr[], int size, int key){
+int binarySearch(int arr[], int size, int key)
+{
     int start = 0;
-    int end = size -1;
-    int mid = start + (end - start)/2;
-    while(start<=end){
-        if(arr[mid] == key){
+    int end = size - 1;
+    int mid = start + (end - start) / 2;
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
             return mid;
         }
-        if(arr[mid] < key){
+        if (arr[mid] < key)
+        {
             start = mid + 1;
         }
-        else{
+        else
+        {
             end = mid - 1;
         }
-        mid = start + (end - start)/2;
-      
+        mid = start + (end - start) / 2;
     }
-    return -1; 
+    return -1;
 }
-int main(){
-    int evenarr[6] = {2,5,7,8,9,12};
-    int oddarr[5] = {5,6,10,15,22};
-     
-    int index = binarySearch(evenarr, 6 , 9);
+int main()
+{
+    int evenarr[6] = {2, 5, 7, 8, 9, 12};
+    int oddarr[5] = {5, 6, 10, 15, 22};
 
-    cout<< "Index  is "<<index;
+    int index = binarySearch(evenarr, 6, 9);
+
+    cout << "Index  is " << index;
 }
