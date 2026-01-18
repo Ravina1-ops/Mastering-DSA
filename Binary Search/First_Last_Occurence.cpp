aa0591dd3bfbe92a9a6c61b3cabdd043d4082485
Ravina <hema130384@gmail.com>
2026-01-18 18:14:10 +0530

    First and last occurence of element

---
+++ Binary Search/First_Last_Occurence.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int firstoccurence(int arr[], int n, int key){
+    int start = 0;
+    int end = n-1;
+    int mid = start + (end - start)/2;
+    int ans = -1;
+    while(start<=end){
+        if(arr[mid] == key){
+            ans = mid;
+            end = mid -1;
+        }
+        if(arr[mid]>key){
+            end = mid -1;
+        }
+        if(arr[mid]<key){
+            start = mid + 1;
+        }
+        mid = start + (end - start)/2;
+    }
+    return ans;
+}
+int lastoccurence(int arr[], int n, int key){
+    int start = 0;
+    int end = n-1;
+    int mid = start + (end - start)/2;
+    int ans = -1;
+    while(start<=end){
+        if(arr[mid] == key){
+            ans = mid;
+            start = mid + 1;
+        }
+        if(arr[mid]>key){
+            end = mid -1;
+        }
+        if(arr[mid]<key){
+            start = mid + 1;
+        }
+        mid = start + (end - start)/2;
+    }
+    return ans;
+}
+int main(){
+    int arr[5]={1,2,2,3,5};
+    cout<<"first occurence is: "<<firstoccurence(arr,5,2)<<endl;
+    cout<<"last occurence is: "<<lastoccurence(arr,5,2);
+}
