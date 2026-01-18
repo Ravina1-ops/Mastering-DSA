01d0dcfb4022c02d32525a3506a0fd1100544e3b
Ravina <hema130384@gmail.com>
2026-01-18 18:22:09 +0530

    First and last occurence

--- Binary Search/First_Last_Occurence.cpp
+++ Binary Search/First_Last_Occurence.cpp
@@ -43,5 +43,14 @@ int lastoccurence(int arr[], int n, int key){
 int main(){
     int arr[5]={1,2,2,3,5};
     cout<<"first occurence is: "<<firstoccurence(arr,5,2)<<endl;
-    cout<<"last occurence is: "<<lastoccurence(arr,5,2);
+    cout<<"last occurence is: "<<lastoccurence(arr,5,2)<<endl;
+    int arr2[15]={1,2,2,3,5,10,10,10,15,15,18,18,18,18,18};
+    cout<<"first occurence is: "<<firstoccurence(arr2,15,18)<<endl;
+    cout<<"last occurence is: "<<lastoccurence(arr2,15,18)<<endl;
+    int arr3[10]={1,2,2,3,5,7,8,9,10,12};
+    cout<<"first occurence is: "<<firstoccurence(arr3,10,7)<<endl;
+    cout<<"last occurence is: "<<lastoccurence(arr3,10,7)<<endl;
+    int arr4[9]={1,2,3,5,7,8,9,10,12};
+    cout<<"first occurence is: "<<firstoccurence(arr4,9,4)<<endl;
+    cout<<"last occurence is: "<<lastoccurence(arr4,9,4)<<endl;
 }
