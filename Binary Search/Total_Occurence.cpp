5fd7fe09e5e8c269cd6a4b0bcc27018f7c8e457d
Ravina <hema130384@gmail.com>
2026-01-22 21:49:11 +0530

    Total Occurence of element in array program

---
+++ Binary Search/Total_Occurence.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+int firstoccurence(int arr[], int n, int key){
+    int start = 0;
+    int end = n-1;
+    int mid = start + (end - start)/2;
+    int ans = -1;
+    while(start<=end){
+        if(arr[mid] == key){
+            ans = mid;
+            end = mid -1;
+        }
+        if(arr[mid]>key){
+            end = mid -1;
+        }
+        if(arr[mid]<key){
+            start = mid + 1;
+        }
+        mid = start + (end - start)/2;
+    }
+    return ans;
+}
+int lastoccurence(int arr[], int n, int key){
+    int start = 0;
+    int end = n-1;
+    int mid = start + (end - start)/2;
+    int ans = -1;
+    while(start<=end){
+        if(arr[mid] == key){
+            ans = mid;
+            start = mid + 1;
+        }
+        if(arr[mid]>key){
+            end = mid -1;
+        }
+        if(arr[mid]<key){
+            start = mid + 1;
+        }
+        mid = start + (end - start)/2;
+    }
+    return ans;
+}
+
+int main(){
+    int arr[5]={1,2,2,3,5};
+    cout<<"first occurence is: "<<firstoccurence(arr,5,2)<<endl;
+    cout<<"last occurence is: "<<lastoccurence(arr,5,2)<<endl;
+    int total = (lastoccurence(arr,5,2) - firstoccurence(arr,5,2))+ 1;
+    cout<<"Total occurence: "<<total<<endl;
+    int arr2[15]={1,2,2,3,5,10,10,10,15,15,18,18,18,18,18};
+    cout<<"first occurence is: "<<firstoccurence(arr2,15,18)<<endl;
+    cout<<"last occurence is: "<<lastoccurence(arr2,15,18)<<endl;
+     int total2 = (lastoccurence(arr2,15,18) - firstoccurence(arr2,15,18))+ 1;
+    cout<<"Total occurence: "<<total2<<endl;
+    int arr3[10]={1,2,2,3,5,7,8,9,10,12};
+    cout<<"first occurence is: "<<firstoccurence(arr3,10,7)<<endl;
+    cout<<"last occurence is: "<<lastoccurence(arr3,10,7)<<endl;
+     int total3 = (lastoccurence(arr3,10,7) - firstoccurence(arr3,10,7))+ 1;
+    cout<<"Total occurence: "<<total3<<endl;
+    int arr4[9]={1,2,3,5,7,8,9,10,12};
+    cout<<"first occurence is: "<<firstoccurence(arr4,9,4)<<endl;
+    cout<<"last occurence is: "<<lastoccurence(arr4,9,4)<<endl;
+     int total4 = (lastoccurence(arr4,9,4) - firstoccurence(arr4,9,4))+ 1;
+    cout<<"Total occurence: "<<total4<<endl;
+}
