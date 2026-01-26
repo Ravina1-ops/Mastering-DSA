126a232e6b433f24af64363fe49db4f28499ea63
Ravina <hema130384@gmail.com>
2026-01-26 14:43:00 +0530

    Book allocation  problem

---
+++ Binary Search/Book_Allocation_Problem.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+bool isPossible(int arr[], int n, int m, int mid){
+    int StudentCount = 1;
+    int pageSum = 0;
+    for(int i = 0; i<n;i++){
+        if(pageSum + arr[i] <= mid){
+            pageSum += arr[i];
+        }
+        else{
+            StudentCount ++;
+            if(StudentCount > m || arr[i] > mid){
+            return false;
+        }
+        pageSum = arr[i];
+    }
+    }
+    return true;
+}
+
+int allocate(int arr[], int n , int m ){
+    int s = 0;
+    int sum = 0;
+    int ans = -1;
+    for(int i = 0;i<n;i++){
+        sum += arr[i];}
+
+    int e = sum;
+    int mid = s+(e-s)/2;
+    while(s<=e){
+        if(isPossible(arr,n,m,mid)){
+            ans = mid;
+            e = mid - 1;
+        }
+        else{
+            s = mid + 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans; 
+}
+
+int main(){
+    int arr[4]={12,34,67,90};
+    int books = allocate(arr,4,2);
+    cout<<"Mid is: "<<books<<endl;
+}
