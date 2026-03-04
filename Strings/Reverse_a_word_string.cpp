79e5018178da37958668fcfc7e4b15ef4c8aef02
Ravina <hema130384@gmail.com>
2026-03-04 16:15:20 +0530

    Reverse a word in a string leetcode question

---
+++ Strings/Reverse_a_word_string.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void reverseword(string& c){
+    for(int i =0;i<c.length();i++){
+        
+    }
+}
+
+int main(){
+
+
+
+    return 0;
+}
