c70b0b19e2f1fa9a2c61baeba023a491de36d0a9
Ravina <hema130384@gmail.com>
2026-03-11 21:23:20 +0530

    code

---
+++ Strings/Replace_Space_without_extra_string_inPlace.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+string replacespace(string s){
+   int spacecount = 0;
+   for(char c:s){
+    if(c == ' '){
+        spacecount++;
+    }
+   }
+   int oldlen
+}
+
+int main(){
+    string s = {"My name is Ravina"};
+    cout<<replacespace(s);
+    return 0;
+}
