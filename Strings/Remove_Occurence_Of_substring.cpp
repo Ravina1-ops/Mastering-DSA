03e01dbd062cefbe1491e1a6f3ba48b0f088b737
Ravina <hema130384@gmail.com>
2026-03-11 21:15:33 +0530

    remove occ code

---
+++ Strings/Remove_Occurence_Of_substring.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+string removeOccurence(string s, string part){
+    while(s.length()!=0&& s.find(part)< s.length()){
+        s.erase(s.find(part),part.length());
+    }
+    return s;
+}
