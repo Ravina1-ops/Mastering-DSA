afd4208edb715c642c4c616060070c5c007cf238
Ravina <hema130384@gmail.com>
2026-03-17 19:31:57 +0530

    Printing and taking inout for 2d array as row wise and column wise

---
+++ 2D Array/Intro_Of_2D_Array.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // create a 2D array 
+    int arr[3][4];
+
+    // row wise input 
+    for(int i = 0; i<3;i++){
+        for(int j = 0; j<4;j++){
+            cin>>arr[i][j]; 
+        }
+    }
+    
+    for(int i = 0;i<3;i++){
+    for(int j = 0;j<4;j++){
+        cout<<arr[i][j]<<" ";
+    }
+    cout<<endl;
+   }
+
+    // column wise input
+    for(int i = 0; i<4;i++){
+        for(int j = 0; j<3;j++){
+            cin>>arr[j][i]; 
+        }
+    }
+
+   for(int i = 0;i<3;i++){
+    for(int j = 0;j<4;j++){
+        cout<<arr[i][j]<<" ";
+    }
+    cout<<endl;
+   }
+   return 0;
+}
