82802a01ee6c080345e6df3cad8cd6adbf3aa212
Ravina <hema130384@gmail.com>
2026-03-21 07:44:50 +0530

    comments in homework question

---
+++ 2D Array/Rotate_matrix_by_90_degree.cpp
@@ -0,0 +1,8 @@
+// given a matrix and rotate it by 90 degree 
+/*
+ given matrix = rotated matrix
+1 2 3                       7 4 1 
+4 5 6    = (by 90 degree)   8 5 2
+7 8 9                       9 6 3
+ 
+*/              
