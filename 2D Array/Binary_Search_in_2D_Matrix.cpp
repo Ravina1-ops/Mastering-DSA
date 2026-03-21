1607eb5f940d3d7ec44891cc303e9fca126bb3b0
Ravina <hema130384@gmail.com>
2026-03-21 08:11:25 +0530

    binary search half done question

---
+++ 2D Array/Binary_Search_in_2D_Matrix.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+bool searchMatrix(vector<vector<int>> &matrix,int target)
+{
+    int row = matrix.size();
+    int column = matrix[0].size();
+    int start = 0;
+    int end = row*column - 1;
+    int mid = start + (end - start) / 2;
+    while (start <= end)
+    {
+        int element = matrix[mid/column][mid%column];
+        if (element == target)
+        {
+            return 1;
+        }
+        if (element < target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+    return 0;
+}
+int main()
+{
+     int row, column;
+    cout << "Enter number of rows: ";
+    cin >> row;
+    cout << "Enter number of cols: ";
+    cin >> column;
+    vector<vector<int>> arr(row, vector<int>(column));
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    cout << "\nMatrix:\n";
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    vector<int> result = searchMatrix(arr,6);
+    cout << "\nSpiral Order: ";
+    for (int val : result)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+
+return 0;
+}
