f5ff94baf9160e42bbc2e124681883b8e5d735f2
Ravina <hema130384@gmail.com>
2026-03-23 19:09:40 +0530

    que sol

--- 2D Array/Rotate_matrix_by_90_degree.cpp
+++ 2D Array/Rotate_matrix_by_90_degree.cpp
@@ -6,5 +6,40 @@
 7 8 9                       9 6 3
  
 */              
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotate90(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+
+    // Step A: Transpose
+    for(int i = 0; i < n; i++) {
+        for(int j = i; j < n; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    // Step B: Reverse each row
+    for(int i = 0; i < n; i++) {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+int main() {
+    vector<vector<int>> mat = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    rotate90(mat);
+
+    // Print rotated matrix
+    for(auto row : mat) {
+        for(auto val : row) cout << val << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
