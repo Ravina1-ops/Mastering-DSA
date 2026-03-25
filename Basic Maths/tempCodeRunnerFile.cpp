949ff3f900953b9980c49a795869abe951366925
Ravina <hema130384@gmail.com>
2026-03-25 18:55:38 +0530

    count prime numbers upto a certain digit

---
+++ Basic Maths/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+int countPrime(int n){
+//     int count = 0;
+//     for(int i = 2; i<n; i++){
+//         if(isprime(i)){
+//             count++;
+//         }
+//         return count;
+//     }
+// }
