5a1f22a424fd3e11a0d1b390ae0c9709419b34e3
Ravina <hema130384@gmail.com>
2026-03-26 19:08:42 +0530

    fast exponentn value using lon long

--- Basic Maths/Modular_Exponentiation.cpp
+++ Basic Maths/Modular_Exponentiation.cpp
@@ -5,14 +5,22 @@ int modularExponentiation(int x,int n, int m){
     while(n>0){
         if(n&1){
             // odd hai 
-            res = ((res)%m * (x)%m)%m;
+            res = ( 1LL * (res)* (x)%m)%m;
         }
-        x = ((x)%m * (x)%m)%m;
+        x = ( 1LL * (x)%m * (x)%m)%m;
         n = n>>1;
     }
     return res;
 }
 
 int main(){
+ int x,n,m;
+ cout<<"Enter base value: ";
+ cin>>x;
+ cout<<"Enter Power value: ";
+ cin>>n;
+ cout<<"Enter modular value: ";
+ cin>>m;
 
+ cout<<"Modular Exponentiation : "<<modularExponentiation(x,n,m);
 }
