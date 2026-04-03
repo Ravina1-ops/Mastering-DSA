9e08687bf146b76b48b3ca7c832bb51626ad0c6b
Ravina <hema130384@gmail.com>
2026-04-03 21:08:27 +0530

    functions passing in pointers

--- Pointers/Function_In_Pointers.cpp
+++ Pointers/Function_In_Pointers.cpp
@@ -12,6 +12,15 @@ void update(int *p){
     *p = *p+1;
 
 }
+
+int getsum(int *arr, int n){
+    int sum = 0;
+    for(int i = 0;i<n;i++){
+        sum += arr[i];
+
+    }
+    return sum;
+}
 int main(){
     int val = 89;
     int *ptr = &val;
@@ -19,5 +28,12 @@ int main(){
     print(ptr);
     update(ptr);
     cout<<"After: "<<*ptr<<endl;
+
+
+    int arr[6] = {1,2,3,4,5,8};
+
+    cout<<"sum is :"<< getsum(arr+3,3);
+    // through using pointer in function and array we find any operation on a 
+    // specific psrt of array like above we find sum of only last 3 elements
     return 0;
 }
