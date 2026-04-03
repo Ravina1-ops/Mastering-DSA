bf99ce9a19a9ea28ca863769204dc7f1047a5b30
Ravina <hema130384@gmail.com>
2026-04-03 21:04:50 +0530

    functions in pointers

--- Pointers/Function_In_Pointers.cpp
+++ Pointers/Function_In_Pointers.cpp
@@ -6,11 +6,18 @@ void print(int *p){
     cout<< p <<endl; // print address
 }
 
+void update(int *p){
+    p = p+1;
+    cout<<"inside update: "<<p<< endl;
+    *p = *p+1;
+
+}
 int main(){
     int val = 89;
     int *ptr = &val;
-
+    cout<<"before: "<<*ptr<<endl;
     print(ptr);
-    
+    update(ptr);
+    cout<<"After: "<<*ptr<<endl;
     return 0;
 }
