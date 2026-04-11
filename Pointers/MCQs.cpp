0dae578afc7a30406d18dcc350069a64840f6c61
Ravina <hema130384@gmail.com>
2026-04-11 19:33:27 +0530

    3rd 4th question

--- Pointers/MCQs.cpp
+++ Pointers/MCQs.cpp
@@ -23,5 +23,23 @@ cout<<first <<endl;
 output = 7
 */
 
+/*3.
+int first = 8;
+int *p = &first;
+cout<< (*p)++ <<" ";
+cout<< first << endl;
+
+output = 8 9 because of post increment
+*/
+
+/*4.
+int *p = 0;
+int first = 110;
+*p = first ;
+cout<< *p <<endl;
+
+output = segmentation fault as p = &first is written for output */
+
+
 return 0;
 }
