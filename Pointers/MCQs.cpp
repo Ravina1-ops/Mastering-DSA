ed400a57c1e3dd817b4a162e7da2fcbebd631284
Ravina <hema130384@gmail.com>
2026-04-11 18:37:55 +0530

    2 ques solve

---
+++ Pointers/MCQs.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /* 1. 
+int first= 8;
+int second = 18;
+int *ptr = &second;
+*ptr = 9;
+cout << first <<" "<<second <<endl;
+
+output = 8 9 
+*/
+
+/* 2.
+
+int first = 6;
+int *p = &first;
+int *q = p;
+(*q)++;
+
+cout<<first <<endl;
+
+output = 7
+*/
+
+return 0;
+}
