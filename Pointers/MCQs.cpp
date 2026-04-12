9f67c82f2a435fb87e10f2fd4f255e72308fd014
Ravina <hema130384@gmail.com>
2026-04-12 12:35:38 +0530

    MCQ questions related to pointers and double pointer solve

--- Pointers/MCQs.cpp
+++ Pointers/MCQs.cpp
@@ -40,6 +40,46 @@ cout<< *p <<endl;
 
 output = segmentation fault as p = &first is written for output */
 
+/*5. 
+int first = 8;
+int second = 11;
+int *third = &second;
+first = *third;
+*third = *third +2;
+cout<< first <<" "<<second<<endl;
+
+output = 11 13 */
+
+/*6.
+float f = 12.5;
+float p = 21.5;
+float *ptr = &f;
+(*ptr)++;
+*ptr = p;
+cout<< *ptr <<" " << f << " " << p << endl;
+
+output  = 21.5 21.5 21.5 
+*/
+
+/* 7. 
+int arr[5];
+int *ptr;
+cout<< sizeof(arr) <<" " <<sizeof(ptr) << endl;
+output = 20 4
+*/
+
+/*8.
+int arr[] = {11,21,13,14};
+cout<< *(arr) << " " <<*(arr+1) <<endl;
+output = 11 21
+*/
+
+/* 9.
+int arr[6] = {11 , 12 , 31};
+cout<< arr << " "<< &arr << endl;
+output both print same address
+*/
+
 
 return 0;
 }
