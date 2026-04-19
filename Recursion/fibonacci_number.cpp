bb9a8392dbd7be8be710108fb7c5a64eef4156de
Ravina <hema130384@gmail.com>
2026-04-19 14:01:05 +0530

    fibonnacci series with recursion

---
+++ Recursion/fibonacci_number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int fibo(int n){
+    if(n == 0 ){
+        return 0;
+    }
+    if(n == 1){
+        return 1;
+    }
+  int ans =  fibo(n-1)+fibo(n-2);
+  return ans;
+}
+
+int main(){
+    int ans = fibo(4);
+    cout<< ans << endl;
+    return 0;
+}
