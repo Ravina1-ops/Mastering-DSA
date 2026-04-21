e1b99aa1e28057502f7f8d6ff2d35d291129b72b
Ravina <hema130384@gmail.com>
2026-04-21 14:13:19 +0530

    array sorted or not using recursion function solve

--- Recursion/Is_Sorted_array.cpp
+++ Recursion/Is_Sorted_array.cpp
@@ -17,9 +17,15 @@ bool issorted(int arr[], int size){
 int main(){
     int arr[5]={2,4,8,9,10};
     int ans = issorted(arr,5);
-    cout<<"Array is Sorted ? "<<ans;
+    // cout<<"Array is Sorted ? "<<ans;
     int arr2[5]={2,4,11,9,10};
     int ans2 = issorted(arr2,5);
-    cout<<endl<<"Array is Sorted ? "<<ans2;
+    // cout<<endl<<"Array is Sorted ? "<<ans2;
+    if(ans2){
+        cout<< "Array is sorted";
+    }
+    else{
+        cout<<"Array is not sorted";
+    }
     return 0;
 }
