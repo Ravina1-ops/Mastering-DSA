928c375e91e93c4f18c58033f44ead4811955672
Ravina <hema130384@gmail.com>
2026-04-21 15:22:35 +0530

    solve find sum of all elements of array using recursion

---
+++ Recursion/sum_of_Array_elements.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int getsum(int arr[], int size){
+    // base case
+    if(size == 0){
+        return 0;
+    }
+    if(size == 1){
+        return arr[0];
+    }
+    int remainingpart = getsum(arr+1,size-1);
+    int sum = arr[0]+ remainingpart;
+    return sum;
+}
+
+int main(){
+    int arr[5] = {2,3,5,6,8};
+    int size = 5;
+
+    int ans = getsum(arr,size);
+    cout<<"Sum is : "<<ans<<endl;
+    return 0;
+}
