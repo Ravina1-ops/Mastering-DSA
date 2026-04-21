bb3f2a221f342a4be7eef667fb44fed11a15b6b8
Ravina <hema130384@gmail.com>
2026-04-21 14:11:11 +0530

    array sort or not question with recursion function solve

---
+++ Recursion/Is_Sorted_array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+bool issorted(int arr[], int size){
+    // base condition
+    if(size == 0 || size == 1){
+        return true;
+    }
+    if(arr[0] > arr[1]){
+        return false;
+    }
+    else{
+        bool ans = issorted(arr+1, size-1);
+    
+    return ans;}
+}
+
+int main(){
+    int arr[5]={2,4,8,9,10};
+    int ans = issorted(arr,5);
+    cout<<"Array is Sorted ? "<<ans;
+    int arr2[5]={2,4,11,9,10};
+    int ans2 = issorted(arr2,5);
+    cout<<endl<<"Array is Sorted ? "<<ans2;
+    return 0;
+}
