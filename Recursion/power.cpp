b8a6c88cc91e268f52edef925e61d6ac6dba6f32
Ravina <hema130384@gmail.com>
2026-04-26 17:47:00 +0530

    raise to power sum solve

--- Recursion/power.cpp
+++ Recursion/power.cpp
@@ -1,19 +1,29 @@
 #include<iostream>
 using namespace std;
 
-int power(int n){
-    if(n == 0){
+int power(int a , int b){
+    if(b == 0){
         return 1;
     }
-    return 2*power(n-1);
+    if(b == 1){
+        return a;
+    }
+    int ans = power(a , b/2);
+    if(b%2 == 0){
+        return ans * ans;
+    }
+    else{
+        return a * ans * ans;
+    }
+    
 }
 int main(){
-    int  n;
-    cout<<"Enter number: ";
-    cin>>n;
+    int  a,b;
+    cout<<"Enter number(base , power): ";
+    cin>>a>>b;
 
-    int ans = power(n);
-    cout<<"2 Raise to Power "<<n<<" is ";
+    int ans = power(a,b);
+    cout << b <<" power raise to "<< a << " is: "<<endl;
     cout<< ans << endl;
 
     return 0;
